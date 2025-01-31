da8e596eddd3a3204d86f46f08f92bb2e9dcbf91
João Vitor <joaossc.ege@gmail.com>
2025-01-31 14:30:07 -0300

    Prototipação

--- tarefa_alarme.c
+++ tarefa_alarme.c
@@ -12,7 +12,6 @@
 void init_pinos();
 volatile bool leds_ativos = false;  // Flag para evitar múltiplos acionamentos
 int64_t alarm_callback(alarm_id_t id, void *user_data); // Função de callback do alarme
-
 void button_callback(uint gpio, uint32_t events);
 
 int main() {
